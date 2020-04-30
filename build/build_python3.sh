1a51c3f7d1a913f3e4ad392724cddc9ece0ac625
ChaosXu <jun.chaos@gmail.com>
2020-04-30 18:34:33 +0800

    fix:build script

---
+++ build/build_python3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum -y install zlib-devel bzip2-devel openssl-devel \
+ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel \
+db4-devel libpcap-devel xz-devel libffi-devel \
+
+cd Python-3.7.5 \
+&& ./configure --prefix=/usr/local/python3 \
+&& make && make install \
+&& ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 \
+&& ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3 \
+&& python3 -V \
+&& pip3 -V
--- build/prepare.sh
+++ build/prepare.sh
@@ -6,7 +6,7 @@ cd ..
 yum -y install epel-release \
     && yum -y install deltarpm \
     && yum -y install @'Development Tools' \
-    && yum -y install rpm-build yum-utils \
+    && yum -y install rpm-build yum-utils
 
 sed -e 's/@VERSION@/0.0.1/' rhel/openvswitch-fedora.spec.in > /tmp/ovs.spec
-yum-builddep -y /tmp/ovs.spec
+yum-builddep --enablerepo epel -y /tmp/ovs.spec
