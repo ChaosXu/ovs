c7205e2dea00874c18c52fb37f710b21ddfc693b
ChaosXu <jun.chaos@gmail.com>
2020-04-30 22:40:49 +0800

    fix:build in docker

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -euxo pipefail
-
+kversion=3.10.0-1127.el7.x86_64
 cd ..
 
 ./boot.sh
 ./configure
 make rpm-fedora
-make rpm-fedora-kmod
+ll
+#make rpm-fedora-kmod RPMBUILD_OPT='-D "kversion 3.10.0-1062.18.1.el7.x86_64"'
+make rpm-fedora-kmod RPMBUILD_OPT='-D "kversion ${kversion}"'
--- build/prepare.sh
+++ build/prepare.sh
@@ -3,10 +3,11 @@ set -euxo pipefail
 
 cd ..
 
-yum -y install epel-release \
+yum -y install epel-release \m
     && yum -y install deltarpm \
     && yum -y install @'Development Tools' \
-    && yum -y install rpm-build yum-utils
+    && yum -y install rpm-build yum-utils \
+    && yum install kernel-devel.x86_64
 
 sed -e 's/@VERSION@/0.0.1/' rhel/openvswitch-fedora.spec.in > /tmp/ovs.spec
 yum-builddep -y /tmp/ovs.spec
